202c10272a6ddcddea2085095512b9b95c5fe77a
Lucas Minatelli Rezende <78113328+lucasminatelli@users.noreply.github.com>
2021-04-10 10:25:01 -0300

    Delete dcConnection.php

--- src/apis/dbConnection.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin: *');
-
-function conexao()
-{
-    $con = mysqli_connect('localhost', 'id15927647_workedhours_user', 'Minatelli[123]', 'id15927647_workedhours_db');
-    return $con;
-}
